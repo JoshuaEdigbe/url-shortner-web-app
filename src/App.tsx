91a6a2224994b7d4a3993be4bd0d9609879780c5
Joshua Edigbe <joshuae@ebanqo.com>
2023-12-16 17:18:43 +0100

    integrate form components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,83 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+// components
+import SubmitButton from "./components/SubmitButton";
+import UrlInput from "./components/UrlInput";
+
+// utlils
+import UrlShortener from "./utils/url-shortener";
+import { APP_STORAGE_KEY } from "./utils/app-const";
 
 function App() {
+  const shortener = new UrlShortener();
+  const [savedUrls, setSavedUrls] = useState<any>([]);
+
+  useEffect(() => {
+    const existingURLs = JSON.parse(
+      localStorage.getItem(APP_STORAGE_KEY) || "[]"
+    );
+
+    setSavedUrls(existingURLs);
+  }, []);
+
+  const saveUrlToLocalStore = (urlData: {
+    shortUrl: string;
+    userUrl: string;
+    id: string;
+  }) => {
+    const existingURLs = JSON.parse(
+      localStorage.getItem(APP_STORAGE_KEY) || "[]"
+    );
+
+    const upatedURLs = [...existingURLs, urlData];
+    setSavedUrls(upatedURLs);
+
+    localStorage.setItem(APP_STORAGE_KEY, JSON.stringify(upatedURLs));
+  };
+
+  const handleShortenUrl = (userInput: string) => {
+    if (userInput) {
+      const shortUrl = shortener.shortenUrl(userInput);
+
+      // save shortened url
+      saveUrlToLocalStore({
+        userUrl: userInput,
+        shortUrl: shortUrl,
+        id: shortener.generateShortCode(),
+      });
+    }
+  };
+
+  const handleShowOriginalUrl = (userInput: string) => {
+    const foundUrl = savedUrls.filter(
+      (savedUrl: any) => savedUrl?.shortUrl === userInput
+    )[0];
+
+    alert(foundUrl?.userUrl || `Sorry, we can't find the original URL :)`);
+  };
+
   return (
     <div className="App">
-      
+      <UrlInput
+        renderButton={(showShortenUrlButton, userInput, handleResetForm) => (
+          <SubmitButton
+            showShortenUrlButton={showShortenUrlButton}
+            handleShortenUrl={() => {
+              handleShortenUrl(userInput);
+              handleResetForm();
+            }}
+            handleShowOriginalUrl={() => handleShowOriginalUrl(userInput)}
+          />
+        )}
+      />
+      <ul>
+        {savedUrls?.map((savedUrl: any, index: number) => (
+          <li key={savedUrl?.id}>
+            {" "}
+            {savedUrl.userUrl} {savedUrl?.shortUrl}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
